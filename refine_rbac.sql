24cfcc586ae84efc67f8b477cee726dec0bc6477
noeEdwin <edwinnoe658@gmail.com>
2025-12-01 19:54:07 -0600

    Eliminar comentarios en refine_rbac

--- refine_rbac.sql
+++ refine_rbac.sql
@@ -1,6 +1,5 @@
 SET search_path TO fruteria_db;
 
--- 1. Create Group Role
 DO
 $do$
 BEGIN
@@ -19,14 +18,12 @@ BEGIN
 END
 $do$;
 
--- Grant privileges to the group role
 GRANT USAGE ON SCHEMA fruteria_db TO fruteria_admin_role;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA fruteria_db TO fruteria_admin_role;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA fruteria_db TO fruteria_admin_role;
 ALTER DEFAULT PRIVILEGES IN SCHEMA fruteria_db GRANT ALL PRIVILEGES ON TABLES TO fruteria_admin_role;
 ALTER DEFAULT PRIVILEGES IN SCHEMA fruteria_db GRANT ALL PRIVILEGES ON SEQUENCES TO fruteria_admin_role;
 
--- 2. Create User 'edwin'
 DO
 $do$
 BEGIN
@@ -43,17 +40,11 @@ BEGIN
 END
 $do$;
 
--- 3. Modify Employee Table
--- We drop password and last_login as they are no longer needed in the app table
 ALTER TABLE empleado DROP COLUMN IF EXISTS password;
 ALTER TABLE empleado DROP COLUMN IF EXISTS last_login;
 
--- 4. Insert/Update 'edwin' in empleado table
--- We use ON CONFLICT to handle if he already exists (assuming id_e or username is unique)
--- First, let's ensure we have a record for him.
 INSERT INTO empleado (id_e, nombre, turno, salario, username, is_active, is_staff, is_superuser)
 VALUES (100, 'Edwin Noe', 'Matutino', 5000.00, 'edwin', true, true, true)
 ON CONFLICT (id_e) DO UPDATE 
 SET username = 'edwin', nombre = 'Edwin Noe';
 
--- Also ensure username is unique constraint if not already (DDL said it was)
